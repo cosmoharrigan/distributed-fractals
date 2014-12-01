9903c02d34fce336623d904a00106df7bf995372
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-12-01 00:18:17 -0800

    Parameters updated

--- fractal.py
+++ fractal.py
@@ -7,17 +7,6 @@ Author: Cosmo Harrigan (based on code by Jake Vanderplas)
 import numpy as np
 import matplotlib.pyplot as plt
 
-Nx = 300
-Ny = 200
-xmin = -2
-xmax = 1
-ymin = -1
-ymax = 1
-max_iter = 20
-
-dx = (xmax - xmin) * 1. / Nx
-dy = (ymax - ymin) * 1. / Ny
-
 
 def mandel(x, y, max_iter):
     """
@@ -35,6 +24,9 @@ def mandel(x, y, max_iter):
 
 
 def compute_column(Ny, ymin, rpart, max_iter, dy):
+    """
+    Compute one column of the Mandelbrot set
+    """
     vector = np.zeros(Ny, dtype=float)
     for y in range(Ny):
         ipart = ymin + y * dy
@@ -43,17 +35,23 @@ def compute_column(Ny, ymin, rpart, max_iter, dy):
     return vector
 
 
-def compute_region(chunk, cols_per_chunk=25):
+def compute_region(chunk, num_chunks, Nx, Ny, xmin, xmax, ymin, ymax, max_iter):
     """
-    Returns an n x m matrix representing m entire columns
+    Compute multiple columns, each of height Ny, of the Mandelbrot set
+    The number of columns computed is: (Nx / num_chunks)
+    The columns represent the region with the x-offset of (chunk * cols_per_chunk)
     """
+
+    cols_per_chunk = Nx / num_chunks
+
+    dx = (xmax - xmin) * 1. / Nx
+    dy = (ymax - ymin) * 1. / Ny
+
     result = np.zeros((Ny, cols_per_chunk), dtype=float)
-    #for x in range(cols_per_chunk):
     for x in range(chunk * cols_per_chunk, chunk * cols_per_chunk + cols_per_chunk):
-        rpart = xmin + x * dx    # (chunk * cols_per_chunk) * dx  # replaced x with pixels_per_chunk * chunk
+        rpart = xmin + x * dx
         local_index = x - chunk * cols_per_chunk
         result[:, local_index] = compute_column(Ny, ymin, rpart, max_iter, dy)
-        # note that, although this is indexed by x, it is actually going to be offset
     return result
 
 
@@ -64,16 +62,16 @@ def create_fractal(Nx, Ny, xmin, xmax, ymin, ymax, max_iter):
     image = np.zeros((Ny, Nx), dtype=float)
 
     # find the coarse graining (granularity) of the step sizes
-    dx = (xmax - xmin) * 1. / Nx
-    dy = (ymax - ymin) * 1. / Ny
+    #dx = (xmax - xmin) * 1. / Nx
+    #dy = (ymax - ymin) * 1. / Ny
 
-    # split 300 into 12 25-piece parts
-    total_cols = 300
     num_chunks = 12
-    cols_per_chunk = total_cols / num_chunks
 
-    # todo: support passing the number of columns per chunk
-    regions = map(compute_region, range(num_chunks))
+    # Only 'a' will be an iterable, and the other parameters will remain the same, so in order to use map,
+    # we need to wrap the compute_region function in a one-parameter lambda function:
+    compute_region_lambda = lambda chunk: compute_region(chunk, num_chunks, Nx, Ny, xmin, xmax, ymin, ymax, max_iter)
+
+    regions = map(compute_region_lambda, range(num_chunks))
 
     combined = np.hstack(regions)
     plt.imshow(combined, cmap=plt.cm.jet)
@@ -82,7 +80,16 @@ def create_fractal(Nx, Ny, xmin, xmax, ymin, ymax, max_iter):
     return image
 
 if __name__ == "__main__":
-    image = create_fractal(300, 200, -2, 1, -1, 1, 20)  # 300, 200, -2, 1, -1, 1, 20
+    # Choose the parameters
+    Nx = 300
+    Ny = 200
+    xmin = -2
+    xmax = 1
+    ymin = -1
+    ymax = 1
+    max_iter = 20
+
+    image = create_fractal(Nx, Ny, xmin, xmax, ymin, ymax, max_iter)
 
     print image.shape
     print image.max()
